5b1c1be6aa5cc0774cddbe47efa63b8aff64f950
Alex Kimathi <keemalekie@gmail.com>
2024-01-21 01:29:17 -0800

    created the Delete and update methods

--- lib/pages/add_todo.dart
+++ lib/pages/add_todo.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
-  const AddTodoPage({super.key});
+  final Map? todo;
+  const AddTodoPage({
+    super.key,
+    this.todo,
+  });
 
   @override
   State<AddTodoPage> createState() => _AddTodoPageState();
@@ -14,11 +18,30 @@ class _AddTodoPageState extends State<AddTodoPage> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
 
+  bool isEdit = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    final todo = widget.todo;
+    if(todo != null){
+      isEdit = true;
+      final title = todo['title'];
+      final description = todo['description'];
+      titleController.text = title;
+      descriptionController.text = description;
+
+    }
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add Todo'),
+        title: Text(
+            isEdit? 'Edit Todo' : 'Add Todo'),
       ),
       body: ListView(
         padding: EdgeInsets.all(20),
@@ -36,10 +59,53 @@ class _AddTodoPageState extends State<AddTodoPage> {
             maxLines: 8,
           ),
           SizedBox(height: 20),
-          ElevatedButton(onPressed: submitData, child: Text('Submit'))
+          ElevatedButton(
+              onPressed: isEdit ? updateData :submitData,
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+                isEdit ? 'Update' : 'Submit'),
+          ))
         ],
       ),
     );
+  }
+
+  Future<void> updateData() async {
+    final todo = widget.todo;
+    if(todo == null){
+      print ('you cannot update');
+      return;
+    }
+    final id = todo['_id'];
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+
+    };
+    // Submit
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+
+    // Convert the 'body' map to a JSON-encoded string
+    final encodedBody = jsonEncode(body);
+
+    // Use 'http.post' with the correct parameters
+    final response = await http.put(
+      uri,
+      body: encodedBody,
+      headers: {'Content-Type': 'application/json'},
+    );
+    // Show success
+    if (response.statusCode == 200) {
+      showSuccessMessage('updated successfully');
+    } else {
+      showErrorMessage('updating failed');
+    }
+
   }
 
   Future<void> submitData() async {
